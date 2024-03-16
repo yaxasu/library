baa85ddff2c4e661261a619a4f6305fac15192ed
Nish <nishxpatel@gmail.com>
2024-03-16 14:18:03 -0400

    testing

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+let modal = document.getElementById("myModal");
+let addBookButton = document.getElementById("addBookButton");
+
+addBookButton.addEventListener('click', handleAddBookClick);
+
+addBookButton.onclick = function(){
+    modal.style.display = "block";
+
+}
+
+
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+    
+}
